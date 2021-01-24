b0fa9a072630ffd706a15208574d8adf036929ef
ivansertic <8wY7kunf>
2021-01-24 23:37:55 +0100

    chore: added username check

--- app/src/main/java/com/ivansertic/magictournament/models/User.kt
+++ app/src/main/java/com/ivansertic/magictournament/models/User.kt
@@ -3,9 +3,9 @@ package com.ivansertic.magictournament.models
 import com.ivansertic.magictournament.models.enums.UserType
 
 data class User(
-        var id: String,
-        var email: String,
-        var username: String,
-        var password: String,
-        var type: UserType
+        var id: String = "",
+        var email: String = "",
+        var username: String = "",
+        var password: String = "",
+        var type: UserType = UserType.HOST
 )
--- app/src/main/java/com/ivansertic/magictournament/repositories/AuthRepository.kt
+++ app/src/main/java/com/ivansertic/magictournament/repositories/AuthRepository.kt
@@ -5,35 +5,59 @@ import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.tasks.Task
 import com.ivansertic.magictournament.activities.Register
 import com.ivansertic.magictournament.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
 import com.ivansertic.magictournament.models.enums.UserType
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.tasks.await
 
 class AuthRepository {
 
     private val firebaseAuth = FirebaseAuth.getInstance()
-    private val firebaseDatabase = FirebaseDatabase.getInstance().reference
+    private val firebaseDatabase = FirebaseFirestore.getInstance()
     val status = MutableLiveData<String>()
 
     fun register(email: String, username: String, password: String, userType: UserType) {
 
-        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
-            if(it.isSuccessful){
-                val user = User(this.firebaseAuth.currentUser!!.uid,email,username,password,userType)
-                firebaseDatabase.child("users").child(user.id).setValue(user)
-                this.status.value = "Registration Successful"
-            }else{
-                this.status.value = "Opps! Something went wrong!"
+        GlobalScope.launch(Dispatchers.IO) {
+            val exists: Boolean = checkUsername(username)
+
+            if(exists){
+                withContext(Main){
+                    status.value = "Username already exists"
+                }
+                return@launch
+            }
+
+            val newUser = firebaseAuth.createUserWithEmailAndPassword(email,password).await()
+            val user = User(newUser.user!!.uid,email,username,password,userType)
+            firebaseDatabase.collection("users").document(user.id).set(user)
+
+            withContext(Main){
+                status.value = "Registration successful"
             }
         }
+    }
+
+    private suspend fun checkUsername(username: String):Boolean {
+        var users: MutableList<User> = mutableListOf()
+        val collection = firebaseDatabase.collection("users").get().await()
+        for (document in collection){
+            users.add(document.toObject(User::class.java))
+        }
+
+        if(users.any { user -> user.username == username }){
+            return true
+        }
+
+        return false
     }
 
 }
